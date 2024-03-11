e90de1e2ff06f48cfb2908ca054b8bd9f08bd149
putridia <putridia710@gmail.com>
2024-03-11 23:45:13 +0700

    Add function to group numbers based on target multiples

---
+++ 07_Data Structure and Algorithm/Praktikum/Praktikum/Praktikum/Praktikum/Praktikum/Soal_Prioritas1_no.1.py
@@ -0,0 +1,13 @@
+# Program Mengelompokkan Bilangan berdasarkan target Kelipatan
+def group_numbers(numbers, target):
+    divisible = []
+    not_divisible = []
+    for num in numbers:
+        if num % target == 0:
+            divisible.append(num)
+        else:
+            not_divisible.append(num)
+    return [divisible, not_divisible]
+
+print(group_numbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3)) # [[3, 6, 9], [1, 2, 4, 5, 7, 8]]
+print(group_numbers([23, 15, 19, 20, 75, 30, 45], 5)) # [[15, 20, 75, 30, 45], [23, 19]]
